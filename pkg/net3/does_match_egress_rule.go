c233e7d3383337e7216fb6a3ba1cc62b844d2060
Mathis Kretz <mathis@bespinian.io>
2020-08-14 15:56:30 +0200

    Check egress network policy match

--- pkg/net3/does_match_egress_rule.go
+++ pkg/net3/does_match_egress_rule.go
@@ -1,11 +1,50 @@
 package net3
 
 import (
+	"context"
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // doesMatchEgressRule checks if a destination matches a network
 // policy egress rule.
-func doesMatchEgressRule(rule networkingv1.NetworkPolicyEgressRule, destination Destination) bool {
-	return true
+func (n *net3) doesMatchEgressRule(rule networkingv1.NetworkPolicyEgressRule, dest *v1.Pod, port int) (bool, error) {
+	doesPortMatch := false
+	for _, p := range rule.Ports {
+		if p.Port.IntValue() == port {
+			doesPortMatch = true
+			break
+		}
+	}
+	if !doesPortMatch {
+		return false, nil
+	}
+
+	for _, from := range rule.To {
+		if from.PodSelector != nil {
+			if !doesMatchSelector(from.PodSelector.MatchLabels, dest.Labels) {
+				continue
+			}
+		}
+		if from.NamespaceSelector != nil {
+			ns, err := n.k8s.CoreV1().Namespaces().Get(context.Background(), dest.Namespace, metav1.GetOptions{})
+			if err != nil {
+				return false, fmt.Errorf("error getting destination namespace %q: %w", dest.Namespace, err)
+			}
+			if !doesMatchSelector(from.NamespaceSelector.MatchLabels, ns.Labels) {
+				continue
+			}
+		}
+		// if from.IpBlock != nil {
+		//	if !doesMatchSelector(from.PodSelector.MatchLabels, dest.Labels) {
+		//		continue
+		//	}
+		// }
+		return true, nil
+	}
+
+	return false, nil
 }
