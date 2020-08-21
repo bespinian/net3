f9ec549890360ad6f68d0d2669afb4ea3b0f5700
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2020-08-21 14:57:11 +0200

    Implement IP block check for network policies

--- pkg/net3/does_match_selector.go
+++ pkg/net3/does_match_selector.go
@@ -2,15 +2,10 @@ package net3
 
 // doesMatchSelector checks if a set of labels matches a selector.
 func doesMatchSelector(selector, labels map[string]string) bool {
-	if len(selector) == 0 {
-		return true
-	}
-
 	for k, v := range selector {
 		if labels[k] != v {
 			return false
 		}
 	}
-
 	return true
 }
