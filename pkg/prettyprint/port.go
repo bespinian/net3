3eedd35b03d8d2d0c465ee48c82ceaeef3cb8849
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2020-08-21 15:10:20 +0200

    Make print prettier

--- pkg/prettyprint/port.go
+++ pkg/prettyprint/port.go
@@ -1,1 +1,23 @@
 package prettyprint
+
+import (
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+func portString(protocol v1.Protocol, port intstr.IntOrString) string {
+	if port.IntVal == 0 {
+		if port.StrVal == "" {
+			return "undefined"
+		}
+		return port.StrVal
+	}
+
+	str := fmt.Sprintf("%s:%v", protocol, port.IntVal)
+	if port.StrVal != "" {
+		str += fmt.Sprintf(" (%s)", port.StrVal)
+	}
+	return str
+}
