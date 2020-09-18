7bf22842ee6e7cb35f38e862235acbce28ceeff0
Mathis Kretz <mathis@bespinian.io>
2020-09-18 16:42:00 +0200

    Add logic for looking up owner of service pods

--- pkg/net3/log.go
+++ pkg/net3/log.go
@@ -2,11 +2,31 @@ package net3
 
 import "fmt"
 
-// Topo lists the topology of a connection path.
+// Log redeploys pods with a proxy container which logs all requests to the specified port
 func (n *net3) Log(namespace, dest string, port int32) error {
 	svcPods, err := n.getServicePods(namespace, dest)
 	if err != err {
 		return fmt.Errorf("error getting pods for service %q in namespace %q: %w", dest, namespace, err)
+	}
+
+	if len(svcPods) <= 0 {
+		return fmt.Errorf("service %q has no pods", dest)
+	}
+
+	samplePod := svcPods[0]
+
+	// find owner of pods
+	if len(samplePod.GetOwnerReferences()) > 0 {
+		// pod is owned by something
+		ownerRef := samplePod.GetOwnerReferences()[0]
+		if ownerRef.Kind == "ReplicaSet" {
+			// look up the ReplicaSet and check whether it is owned by a Deployment
+
+		} else {
+			// any other possibilities?
+		}
+	} else {
+		// pod has been created manually, conclude that all others have too and replace them one by one
 	}
 
 	for _, p := range svcPods {
