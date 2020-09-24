9be29e50d58ceabe14088c7bf30f4d01b4db4d7a
Mathis Kretz <mathis@bespinian.io>
2020-09-25 00:10:42 +0200

    Add deployment lookup

--- pkg/net3/log.go
+++ pkg/net3/log.go
@@ -1,7 +1,10 @@
 package net3
 
 import (
+	"context"
 	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Log redeploys pods with a proxy container which logs all requests to the specified port.
@@ -20,12 +23,31 @@ func (n *net3) Log(namespace, dest string, port int32) error {
 	// find owner of pods
 	if len(samplePod.GetOwnerReferences()) > 0 {
 		// pod is owned by something
-		ownerRef := samplePod.GetOwnerReferences()[0]
-		if ownerRef.Kind == "ReplicaSet" {
+		podOwnerRef := samplePod.GetOwnerReferences()[0]
+		if podOwnerRef.Kind == "ReplicaSet" {
 			// look up the ReplicaSet and check whether it is owned by a Deployment
-
+			replicaSet, err := n.k8s.AppsV1().ReplicaSets(namespace).Get(context.Background(), podOwnerRef.Name, metav1.GetOptions{})
+			if err != nil {
+				return fmt.Errorf("no ReplicaSet %q matching owner ref found: %w", podOwnerRef.Name, err)
+			}
+			if len(replicaSet.GetOwnerReferences()) > 0 {
+				// ReplicaSet is owned by something
+				replicaSetOwnerRef := replicaSet.GetOwnerReferences()[0]
+				if replicaSetOwnerRef.Kind == "Deployment" {
+					// Retrieve the Deployment, add the proxy container to the pod spec and apply
+					deployment, err := n.k8s.AppsV1().Deployments(namespace).Get(context.Background(), replicaSetOwnerRef.Name, metav1.GetOptions{})
+					if err != nil {
+						return fmt.Errorf("no Deployment %q matching owner ref found: %w", replicaSetOwnerRef.Name, err)
+					}
+					fmt.Println(deployment.Name)
+				} else {
+					// any other possibilities (don't know any)?
+				}
+			} else {
+				// ReplicaSet has been created manually. Add the proxy container to the pod spec and apply
+			}
 		} else {
-			// any other possibilities?
+			// any other possibilities (don't know any)?
 		}
 	} else {
 		// pod has been created manually, conclude that all others have too and replace them one by one
