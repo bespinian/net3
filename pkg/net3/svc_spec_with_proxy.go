39da05b85dd52583bd98bb65b887b111f2853e3d
Mathis Kretz <mathis@bespinian.io>
2020-10-14 11:44:37 +0200

    Make container name and image configurable

--- pkg/net3/svc_spec_with_proxy.go
+++ pkg/net3/svc_spec_with_proxy.go
@@ -1,9 +1,27 @@
 package net3
 
 import (
+	"fmt"
+
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
+
+const svcAnnotationNamePrefix = "net3.bespinian.io/proxy-container-name"
+
+func svcWithProxy(svc v1.Service, containerName string, port, newTargetPort int32) v1.Service {
+	svc.Spec = svcSpecWithTargetPort(svc.Spec, port, newTargetPort)
+	annotationName := fmt.Sprintf("%s-%v", svcAnnotationNamePrefix, port)
+	svc.Annotations[annotationName] = containerName
+	return svc
+}
+
+func svcWithoutProxy(svc v1.Service, port, originalTargetPort int32) v1.Service {
+	svc.Spec = svcSpecWithTargetPort(svc.Spec, port, originalTargetPort)
+	annotationName := fmt.Sprintf("%s-%v", svcAnnotationNamePrefix, port)
+	delete(svc.Annotations, annotationName)
+	return svc
+}
 
 func svcSpecWithTargetPort(service v1.ServiceSpec, port, newTargetPort int32) v1.ServiceSpec {
 	updatedPorts := make([]v1.ServicePort, 0)
