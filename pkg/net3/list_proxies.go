23677854f1cebb0857cb66dd4b6f41298c6d3115
Lena Fuhrimann <mastertinner@users.noreply.github.com>
2021-10-05 20:09:21 +0200

    Upgrade Go to version 1.17

---
+++ pkg/net3/list_proxies.go
@@ -0,0 +1,6 @@
+package net3
+
+// Logs redeploys pods with a proxy container which logs all requests to the specified port.
+func (n *net3) ListProxies(namespace string, allNamespaces bool, service string) error {
+	return nil
+}
